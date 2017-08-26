739437a0e6eeb430a181722cd6ea916cc769cd1e
Unknown <vinaychandra.dommeti@gmail.com>
2017-08-26 10:51:02 -0700

    Operator &

--- source/Spruce/Tokens/Operators.cs
+++ source/Spruce/Tokens/Operators.cs
@@ -1,50 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Spruce.Tokens
+﻿namespace Spruce.Tokens
 {
-    public class OpEquals : MatchOne {
-        public OpEquals() : base("=") { }
+    public class OpEquals : MatchOne
+    {
+        public OpEquals() : base("=")
+        {
+        }
     }
 
-    public class OpSlashBack : MatchOne {
-        public OpSlashBack() : base(@"\") { }
+    public class OpSlashBack : MatchOne
+    {
+        public OpSlashBack() : base(@"\")
+        {
+        }
     }
 
-    public class OpSlashFwd : MatchOne {
-        public OpSlashFwd() : base("/") { }
+    public class OpSlashFwd : MatchOne
+    {
+        public OpSlashFwd() : base("/")
+        {
+        }
     }
 
-    public class OpPlus : MatchOne {
-        public OpPlus() : base("+") { }
+    public class OpPlus : MatchOne
+    {
+        public OpPlus() : base("+")
+        {
+        }
     }
 
-    public class OpMinus : MatchOne {
-        public OpMinus() : base("-") { }
+    public class OpMinus : MatchOne
+    {
+        public OpMinus() : base("-")
+        {
+        }
     }
 
-    public class OpStar : MatchOne {
-        public OpStar() : base("*") { }
+    public class OpStar : MatchOne
+    {
+        public OpStar() : base("*")
+        {
+        }
     }
 
-    public class OpPipe : MatchOne {
-        public OpPipe() : base("|") { }
+    public class OpPipe : MatchOne
+    {
+        public OpPipe() : base("|")
+        {
+        }
     }
 
-    public class OpTilde : MatchOne {
-        public OpTilde() : base("~") { }
+    public class OpTilde : MatchOne
+    {
+        public OpTilde() : base("~")
+        {
+        }
     }
 
-    public class OpPercent : MatchOne {
-        public OpPercent() : base("%") { }
+    public class OpPercent : MatchOne
+    {
+        public OpPercent() : base("%")
+        {
+        }
     }
 
-    public class OpCaret : MatchOne {
-        public OpCaret() : base("^") { }
+    public class OpCaret : MatchOne
+    {
+        public OpCaret() : base("^")
+        {
+        }
     }
 
-    public class OpAt : MatchOne {
-        public OpAt() : base("@") { }
+    public class OpAt : MatchOne
+    {
+        public OpAt() : base("@")
+        {
+        }
+    }
+
+    public class OpAnd : MatchOne
+    {
+        public OpAnd() : base("&")
+        {
+        }
     }
 }
