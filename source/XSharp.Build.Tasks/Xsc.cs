1035abe24202fd73935c33ec9e8f12ee6683ce2f
José Pedro <jpedrolmail@gmail.com>
2017-08-26 18:41:29 +0100

    X# build tasks.

--- source/XSharp.Build.Tasks/Xsc.cs
+++ source/XSharp.Build.Tasks/Xsc.cs
@@ -1,80 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
 namespace XSharp.Build.Tasks
 {
-    public class Xsc : Task
+    public class Xsc : ToolTask
     {
         [Required]
-        public ITaskItem[] XSharpFiles { get; set; }
+        public ITaskItem[] InputFiles { get; set; }
 
         public bool Append { get; set; } = false;
 
         public string OutputFile { get; set; } = null;
 
-        public override bool Execute()
+        protected override string ToolName => "xsc.exe";
+
+        protected override string GenerateFullPathToTool()
         {
-            try
+            if (String.IsNullOrWhiteSpace(ToolPath))
             {
-                var xArgs = new List<string>();
-
-                foreach (var xFile in XSharpFiles)
-                {
-                    xArgs.Add($"\"{xFile.GetMetadata("FullPath")}\"");
-                }
-
-                if (Append)
-                {
-                    xArgs.Add("-Append");
-                }
-
-                if (OutputFile != null && !String.IsNullOrWhiteSpace(OutputFile))
-                {
-                    OutputFile = Path.GetFullPath(OutputFile);
-                    xArgs.Add($"-Out:\"{OutputFile}\"");
-                }
-
-                xArgs.Insert(0, "xsharp");
-
-                var xPSI = new ProcessStartInfo
-                {
-                    FileName = "dotnet",
-                    Arguments = String.Join(" ", xArgs),
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true
-                };
-
-                var xProcess = new Process
-                {
-                    StartInfo = xPSI
-                };
-
-                Log.LogMessagesFromStream(xProcess.StandardOutput, MessageImportance.Low);
-
-                xProcess.ErrorDataReceived += delegate (object sender, DataReceivedEventArgs e)
-                {
-                    Log.LogError(e.Data);
-                };
-
-                xProcess.Start();
-                xProcess.WaitForExit();
-
-                return true;
+                return Directory.GetCurrentDirectory();
             }
-            catch (Exception e)
-            {
-                Log.LogErrorFromException(e);
-                return false;
-            }
+
+            return Path.GetFullPath(ToolPath);
         }
 
-        private void XProcess_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        protected override string GenerateCommandLineCommands()
         {
-            throw new NotImplementedException();
+            var xBuilder = new CommandLineBuilder();
+
+            if (ToolExe.Equals("dotnet", StringComparison.OrdinalIgnoreCase))
+            {
+                xBuilder.AppendSwitch("xsc.dll");
+            }
+
+            xBuilder.AppendFileNamesIfNotNull(InputFiles, " ");
+            
+            if (Append)
+            {
+                xBuilder.AppendSwitch("-Append");
+            }
+
+            if (OutputFile != null)
+            {
+                xBuilder.AppendSwitch($"-Out:\"${OutputFile}\"");
+            }
+
+            return xBuilder.ToString();
         }
     }
 }
