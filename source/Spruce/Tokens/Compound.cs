68c7fe73553900971288314457a90204dd3d04b7
Kudzu <chad@hower.org>
2017-08-27 15:23:59 -0400

    Compound shell

---
+++ source/Spruce/Tokens/Compound.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spruce.Tokens {
+    public class Compound : Token {
+        protected override object Check(string aText) {
+            throw new NotImplementedException();
+        }
+    }
+}
