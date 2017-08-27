a6b6b42f445be5b26c81c4e3e108595c35b495b2
Unknown <vinaychandra.dommeti@gmail.com>
2017-08-27 14:33:05 -0700

    Added variable definitions to emitters

--- source/XSharp/Emitters.cs
+++ source/XSharp/Emitters.cs
@@ -152,6 +152,18 @@ namespace XSharp
         {
         }
 
+        // const i = 0
+        [Emitter(typeof(Variable), typeof(AlphaNum), typeof(OpEquals), typeof(Int08u))]
+        [Emitter(typeof(Variable), typeof(AlphaNum), typeof(OpEquals), typeof(Int16u))]
+        [Emitter(typeof(Variable), typeof(AlphaNum), typeof(OpEquals), typeof(Int32u))]
+        [Emitter(typeof(Variable), typeof(AlphaNum), typeof(OpEquals), typeof(String))]
+        [Emitter(typeof(Variable), typeof(AlphaNum), typeof(OpEquals), typeof(Constant))]
+        [Emitter(typeof(Variable), typeof(AlphaNum), typeof(OpEquals), typeof(Variable))]
+        [Emitter(typeof(Variable), typeof(AlphaNum), typeof(OpEquals), typeof(VariableAddress))]
+        protected void VariableDefinition(string aVarKeyword, string aVariableName, string oOpEquals, object aVariableValue)
+        {
+        }
+
         // EAX = #constVal
         [Emitter(typeof(RegXX), typeof(OpMath), typeof(Constant))]
         [Emitter(typeof(RegXX), typeof(OpMath), typeof(Variable))]
