4060e5af6bb88028ad322aea91bdcd0c0bee4bc9
José Pedro <jpedrolmail@gmail.com>
2017-09-23 23:10:30 +0100

    X# project system.

---
+++ source/XSharp.ProjectSystem/VS/Build/PublishWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.VisualStudio.PlatformUI;
+
+namespace XSharp.ProjectSystem.VS.Build
+{
+    /// <summary>
+    /// Interaction logic for PublishWindow.xaml
+    /// </summary>
+    public partial class PublishWindow : DialogWindow
+    {
+        public PublishWindow()
+        {
+            InitializeComponent();
+        }
+
+        public PublishWindow(string aHelpTopic) : base(aHelpTopic)
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ source/XSharp.ProjectSystem/VS/Build/XSharpPublishProvider.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Composition;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.ProjectSystem;
+using Microsoft.VisualStudio.ProjectSystem.Build;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace XSharp.ProjectSystem.VS.Build
+{
+    [Export(typeof(IPublishProvider))]
+    [AppliesTo(ProjectCapability.XSharp)]
+    internal class XSharpPublishProvider : IPublishProvider
+    {
+        [Import]
+        private ProjectProperties ProjectProperties { get; set; }
+
+        [Import]
+        private SVsServiceProvider ServiceProvider { get; set; }
+
+        [Import]
+        private IProjectThreadingService ProjectThreadingService { get; set; }
+
+        public async Task<bool> IsPublishSupportedAsync()
+        {
+            // todo: when does this method get called? on load or on project changed too?
+            var xConfiguration = await ProjectProperties.GetConfigurationGeneralPropertiesAsync();
+            return await xConfiguration.OutputType.GetEvaluatedValueAtEndAsync() == "Bootable";
+        }
+
+        public async Task PublishAsync(CancellationToken aCancellationToken, TextWriter aOutputPaneWriter)
+        {
+            await aOutputPaneWriter.WriteLineAsync("Publish successful!");
+        }
+
+        public Task<bool> ShowPublishPromptAsync()
+        {
+            ProjectThreadingService.SwitchToUIThread();
+
+            var xPublishWindow = new PublishWindow();
+            var xResult = xPublishWindow.ShowModal();
+
+            return Task.FromResult(xResult.GetValueOrDefault(false));
+        }
+    }
+}
