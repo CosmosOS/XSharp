c71f229b6c68f686bfce18b819588dae97890799
José Pedro <jpedrolmail@gmail.com>
2017-12-11 01:06:46 +0000

    X# project system.

---
+++ source/XSharp.ProjectSystem/ProjectSystem/VS/Debug/XSharpDebugPageGuidProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Composition;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.ProjectSystem;
+using Microsoft.VisualStudio.ProjectSystem.VS.Debug;
+
+using XSharp.ProjectSystem.VS.PropertyPages;
+
+namespace XSharp.ProjectSystem.VS.Debug
+{
+    [Export(typeof(IDebugPageGuidProvider))]
+    [AppliesTo(ProjectCapability.XSharpAndRunningInVisualStudio)]
+    internal class XSharpDebugPageGuidProvider : IDebugPageGuidProvider
+    {
+        private static readonly Task<Guid> Guid = Task.FromResult(new Guid(DebugPropertyPage.PageGuid));
+
+        public Task<Guid> GetDebugPropertyPageGuidAsync() => Guid;
+    }
+}
---
+++ source/XSharp.ProjectSystem/ProjectSystem/VS/PropertyPages/Views/DebugPropertyPageControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using VSPropertyPages;
+
+namespace XSharp.ProjectSystem.VS.PropertyPages
+{
+    /// <summary>
+    /// Interaction logic for DebugPropertyPageControl.xaml
+    /// </summary>
+    internal partial class DebugPropertyPageControl : WpfPropertyPageUI
+    {
+        public DebugPropertyPageControl()
+        {
+            InitializeComponent();
+        }
+
+        public override Task SetViewModelAsync(PropertyPageViewModel propertyPageViewModel)
+        {
+            DataContext = propertyPageViewModel;
+            return Task.CompletedTask;
+        }
+    }
+}
